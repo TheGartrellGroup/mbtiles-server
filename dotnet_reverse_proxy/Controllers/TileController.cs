c52dd6e7259b06803e4ac4449a95c9786225a268
sainsb <ben.sainsbury@gartrellgroup.com>
2016-11-11 18:17:36 -0800

    Added .NET proxy

---
+++ dotnet_reverse_proxy/Controllers/TileController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+using System.Net;
+
+namespace Tileproxy.Controllers
+{
+    public class TileController : Controller
+    {
+
+        public ActionResult ProxyTile(string service, int x, int y, int z, string format)
+        {
+            //+ Randomize()
+            var mbtiles_server_url = tileproxy.SERVER + ":"+GetPort()+"/" + service + "/" + z + "/" + x + "/" + y +"."+format;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(mbtiles_server_url);
+
+            HttpWebResponse response;
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException we)
+            {
+                //remote url not found, send 404 to client 
+                if (format.ToUpper() == "JPG")
+                {
+                    return File(tileproxy.BLANK_PNG, "image/jpg");
+                } else if (format.ToUpper() == "PNG") {
+                    return File(tileproxy.BLANK_JPEG, "image/png");
+                } else { //blank vector tile?
+                    return File(new byte[0], "application / x - protobuf");
+                }
+            }
+
+            Stream receiveStream = response.GetResponseStream();
+            
+            if(format.ToUpper() == "PBF")
+            {
+                Response.AddHeader("Content-Encoding", "gzip");
+                return new FileStreamResult(receiveStream, "application / x - protobuf");
+            } else {
+                return new FileStreamResult(receiveStream, "image/"+format);
+            }
+        }
+
+        public string GetPort()
+        {
+            var r = new Random();
+            var num = r.Next(0,tileproxy.PORTS.Length);
+            return tileproxy.PORTS[num];
+        }
+    }
+}
---
+++ dotnet_reverse_proxy/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Configuration;
+
+namespace Tileproxy
+{
+
+    public class tileproxy : HttpApplication
+    {
+        
+        public static string SERVER = ConfigurationManager.AppSettings["SERVER"];
+
+        public static string[] PORTS = ConfigurationManager.AppSettings["SERVER"].Split(',');
+        public static byte[] BLANK_PNG = System.Convert.FromBase64String(ConfigurationManager.AppSettings["BLANK_PNG"]);
+
+        public static byte[] BLANK_JPEG = System.Convert.FromBase64String(ConfigurationManager.AppSettings["BLANK_JPEG"]);
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Tiles", // Route name
+                "{service}/{z}/{x}/{y}.{format}", // URL with parameters
+                new { controller = "Tile", action = "ProxyTile"} // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_End()
+        {
+           
+        }
+    }
+}
